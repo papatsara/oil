f234229c932cb7d1b3daf04c1f7d1aaccc2d6898
papatsara <oil_x2-01@hotmail.com>
2016-10-15 15:01:04 +0700

    finsh 15oct

--- app/src/main/java/rtc/papatsara/oiltraffic/MainActivity.java
+++ app/src/main/java/rtc/papatsara/oiltraffic/MainActivity.java
@@ -1,13 +1,50 @@
 package rtc.papatsara.oiltraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.livTraffic);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.dog);
+                mediaPlayer.start();
+
+                //Show Webview
+                String url = "https://youtu.be/h2nIHSgQ2Bs";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            } // onClick
+        });
+
+
+    }   // Main Method
+
+}   // Main Class นี่คือ คลาสหลัก
